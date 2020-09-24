93f2fb9ba096ccfff829069a1f83f6897837e584
Daniele Lo Preiato <lopred@usi.ch>
2020-09-24 15:36:33 +0200

    Parsing degli errori dell'Auth
    Piccola libreria per fare il parsing dei codici di errore dell'Auth

---
+++ src/libs/errorParser.js
@@ -0,0 +1,18 @@
+const errorParser = (e) => {
+    switch (e) {
+        case "auth/email-already-exists":
+            return "La mail fornita risulta già in uso da un altro utente!"
+        case "auth/internal-error":
+            return "Errore di sistema durante la procedura di autenticazione!"
+        case "auth/invalid-argument":
+            return "Errore di sistema: argomento non valido!"
+        case "auth/user-not-found":
+            return "Non è stato trovato alcun account con la mail inserita!"
+        case "auth/invalid-email":
+            return "L'indirizzo mail fornito non è valido!"
+        default:
+            return "Si è verificato un rrore durante la procedura di autenticazione!"
+    }
+}
+
+export default errorParser
