02e9cd90a916dcb530b5e73819888eb1c9bfae7e
Daniele Lo Preiato <lopred@usi.ch>
2020-09-24 15:35:28 +0200

    Import di Firebase
    Import di firebase (db/auth)

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAxnRT7PCOzxVGMCrCeRn-yAjiNc12UO0U",
+    authDomain: "mywork-92c2b.firebaseapp.com",
+    databaseURL: "https://mywork-92c2b.firebaseio.com",
+    projectId: "mywork-92c2b",
+    storageBucket: "mywork-92c2b.appspot.com",
+    messagingSenderId: "409311604436",
+    appId: "1:409311604436:web:8e9564fdd64c4546305541",
+    measurementId: "G-GS7L4PYYSE"
+});
+
+export default firebaseApp;
